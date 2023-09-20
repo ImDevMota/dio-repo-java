871e0525817074f79b086bd218e1d1b95ef987c3
thiago mota <thiago.you23@gmail.com>
2023-09-20 17:37:49 -0300

    Aprendendo a Sintaxe Java Finalizado

---
+++ java-basico/src/stu/mota/metodos/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv(); 
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+
+
+        System.out.println("A TV esta ligada ? " +smartTv.ligada);
+        System.out.println("Canal Atual ? " +smartTv.canal);
+        System.out.println("Volume Atual ? " +smartTv.volume);
+
+        smartTv.mudarCanal(15);
+        System.out.println("Canal Atual ? " +smartTv.canal);
+        
+        smartTv.ligar ();
+        System.out.println("A TV esta ligada agora? " +smartTv.ligada);
+
+        smartTv.desligar ();
+        System.out.println("A TV esta ligada ? " +smartTv.ligada);
+
+    }
+}
